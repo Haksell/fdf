c510a80ea4abb8ccf23117c7ee7f97971b878670
Axel Brisse <axel.brisse@gmail.com>
2023-02-03 05:42:22 +0100

    t_vertex **copy

--- srcs/main.c
+++ srcs/main.c
@@ -6,32 +6,34 @@
 /*   By: axbrisse <axbrisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/12 10:04:35 by axbrisse          #+#    #+#             */
-/*   Updated: 2023/02/03 05:03:41 by axbrisse         ###   ########.fr       */
+/*   Updated: 2023/02/03 05:41:07 by axbrisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include <stdio.h> // TODO
 
-void	translate(t_data *data, double **points, double translation)
+bool	dup_vertices(t_map *map, t_vertex ***copy)
 {
-	int	i;
-	int	j;
+	int	x;
+	int	y;
 
-	i = 0;
-	while (i < data->map.height)
+	if (!init_grid((void ***)copy, map->width, map->height, sizeof(t_vertex)))
+		return (false);
+	y = 0;
+	while (y < map->height)
 	{
-		j = 0;
-		while (j < data->map.width)
+		x = 0;
+		while (x < map->width)
 		{
-			points[i][j] += translation;
-			++j;
+			(*copy)[y][x] = map->vertices[y][x];
+			++x;
 		}
-		++i;
+		++y;
 	}
+	return (true);
 }
 
-void	rotate(t_data *data, double **xs, double **ys)
+/*void	rotate(t_data *data, t_vertex **vertices)
 {
 	int		i;
 	int		j;
@@ -52,9 +54,9 @@ void	rotate(t_data *data, double **xs, double **ys)
 		}
 		++i;
 	}
-}
+}*/
 
-void	scale(t_data *data, double **xs, double **ys)
+void	scale(t_data *data, t_vertex **vertices)
 {
 	int	i;
 	int	j;
@@ -65,45 +67,53 @@ void	scale(t_data *data, double **xs, double **ys)
 		j = 0;
 		while (j < data->map.width)
 		{
-			xs[i][j] *= data->params.scale;
-			ys[i][j] *= data->params.scale;
+			vertices[i][j].x *= data->params.scale;
+			vertices[i][j].y *= data->params.scale;
 			++j;
 		}
 		++i;
 	}
 }
 
+void	translate(t_data *data, t_vertex **vertices)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < data->map.height)
+	{
+		x = 0;
+		while (x < data->map.width)
+		{
+			vertices[y][x].x += data->params.tx;
+			vertices[y][x].y += data->params.ty;
+			++x;
+		}
+		++y;
+	}
+}
+
 int render_frame(t_data *data)
 {
-	double **xs;
-	double **ys;
+	t_vertex **copy;
 
 	if (!data->is_modified)
 		return (EXIT_SUCCESS);
 	black_background(data);
-	if (!init_grid((void ***)&xs, data->map.width, data->map.height, sizeof(double))
-		|| !init_grid((void ***)&ys, data->map.width, data->map.height, sizeof(double)))
+	if (!dup_vertices(&data->map, &copy))
 		close_window(data);
-	for (int y = 0; y < data->map.height; ++y)
-	{
-		for (int x = 0; x < data->map.width; ++x)
-		{
-			xs[y][x] = data->map.vertices[y][x].x;
-			ys[y][x] = data->map.vertices[y][x].y;
-		}
-	}
-	scale(data, xs, ys);
-	rotate(data, xs, ys);
-	translate(data, xs, data->params.tx);
-	translate(data, ys, data->params.ty);
+	//rotate(data, copy);
+	scale(data, copy);
+	translate(data, copy);
 	for (int y = 0; y < data->map.height; ++y)
 	{
 		for (int x = 0; x < data->map.width; ++x)
 		{
 			if (x + 1 < data->map.width)
-				line(data, (int)xs[y][x], (int)ys[y][x], data->map.vertices[y][x].color, (int)xs[y][x + 1], (int)ys[y][x + 1], data->map.vertices[y][x + 1].color);
+				line(data, (int)copy[y][x].x, (int)copy[y][x].y, copy[y][x].color, (int)copy[y][x + 1].x, (int)copy[y][x + 1].y, copy[y][x + 1].color);
 			if (y + 1 < data->map.height)
-				line(data, (int)xs[y][x], (int)ys[y][x], data->map.vertices[y][x].color, (int)xs[y + 1][x], (int)ys[y + 1][x], data->map.vertices[y + 1][x].color);
+				line(data, (int)copy[y][x].x, (int)copy[y][x].y, copy[y][x].color, (int)copy[y + 1][x].x, (int)copy[y + 1][x].y, copy[y + 1][x].color);
 		}
 	}
 	mlx_put_image_to_window(data->mlx, data->win, data->img, 0, 0);
