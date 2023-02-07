8e6f48e805bc6b85a796fd19205371cec89a87d7
Axel Brisse <axel.brisse@gmail.com>
2023-02-07 14:04:32 +0100

    more norm still looks good (untested tho)

--- srcs/put_lines.c
+++ srcs/put_lines.c
@@ -6,7 +6,7 @@
 /*   By: axbrisse <axbrisse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 03:48:33 by axbrisse          #+#    #+#             */
-/*   Updated: 2023/02/07 13:41:54 by axbrisse         ###   ########.fr       */
+/*   Updated: 2023/02/07 14:04:21 by axbrisse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,66 +51,97 @@ static t_int_vertex	create_int_vertex(t_vertex *vertex)
 	return (int_vertex);
 }
 
-void    bresenham(t_data *data, t_int_vertex v0, t_int_vertex v1)
+static t_int_vertex	get_shift_vector(t_int_vertex v0, t_int_vertex v1)
 {
-	const t_int_vertex	dv = {abs(v1.x - v0.x), abs(v1.y - v0.y), abs(v1.z - v0.z), 0};
-	const t_int_vertex	sv = {get_sign(v1.x - v0.x), get_sign(v1.y - v0.y), get_sign(v1.z - v0.z), 0};
-	
-	colorize_pixel(data, &v0, v0.color);
-	if (dv.x >= dv.y && dv.x >= dv.z) {
-		int p1 = 2 * dv.y - dv.x;
-		int p2 = 2 * dv.z - dv.x;
-		while (v0.x != v1.x) {
-			v0.x += sv.x;
-			if (p1 >= 0) {
-				v0.y += sv.y;
-				p1 -= 2 * dv.x;
-			}
-			if (p2 >= 0) {
-				v0.z += sv.z;
-				p2 -= 2 * dv.x;
-			}
-			p1 += 2 * dv.y;
-			p2 += 2 * dv.z;
-			colorize_pixel(data, &v0, lerp_color(v0.color, v1.color, abs(v1.x - v0.x) + abs(v1.y - v0.y) + abs(v1.z - v0.z), dv.x + dv.y + dv.z));
-		}
-	}
-	else if (dv.y >= dv.x && dv.y >= dv.z) {
-		int p1 = 2 * dv.x - dv.y;
-		int p2 = 2 * dv.z - dv.y;
-		while (v0.y != v1.y) {
+	return ((t_int_vertex){get_sign(v1.x - v0.x), get_sign(v1.y - v0.y), get_sign(v1.z - v0.z), 0});
+}
+
+// TODO add start to t_int_vertex
+
+static void	bresenham_x(t_data *data, t_int_vertex v0, t_int_vertex v1, t_int_vertex dv)
+{
+	const t_int_vertex	sv = get_shift_vector(v0, v1);
+	int p1 = 2 * dv.y - dv.x;
+	int p2 = 2 * dv.z - dv.x;
+
+	while (v0.x != v1.x) {
+		v0.x += sv.x;
+		if (p1 >= 0)
+		{
 			v0.y += sv.y;
-			if (p1 >= 0) {
-				v0.x += sv.x;
-				p1 -= 2 * dv.y;
-			}
-			if (p2 >= 0) {
-				v0.z += sv.z;
-				p2 -= 2 * dv.y;
-			}
-			p1 += 2 * dv.x;
-			p2 += 2 * dv.z;
-			colorize_pixel(data, &v0, lerp_color(v0.color, v1.color, abs(v1.x - v0.x) + abs(v1.y - v0.y) + abs(v1.z - v0.z), dv.x + dv.y + dv.z));
+			p1 -= 2 * dv.x;
 		}
-	}
-	else {
-		int p1 = 2 * dv.y - dv.z;
-		int p2 = 2 * dv.x - dv.z;
-		while (v0.z != v1.z) {
+		if (p2 >= 0)
+		{
 			v0.z += sv.z;
-			if (p1 >= 0) {
-				v0.y += sv.y;
-				p1 -= 2 * dv.z;
-			}
-			if (p2 >= 0) {
-				v0.x += sv.x;
-				p2 -= 2 * dv.z;
-			}
-			p1 += 2 * dv.y;
-			p2 += 2 * dv.x;
-			colorize_pixel(data, &v0, lerp_color(v0.color, v1.color, abs(v1.x - v0.x) + abs(v1.y - v0.y) + abs(v1.z - v0.z), dv.x + dv.y + dv.z));
+			p2 -= 2 * dv.x;
 		}
+		p1 += 2 * dv.y;
+		p2 += 2 * dv.z;
+		colorize_pixel(data, &v0, lerp_color(v0.color, v1.color, abs(v1.x - v0.x) + abs(v1.y - v0.y) + abs(v1.z - v0.z), dv.x + dv.y + dv.z));
 	}
+}
+
+static void	bresenham_y(t_data *data, t_int_vertex v0, t_int_vertex v1, t_int_vertex dv)
+{
+	const t_int_vertex	sv = get_shift_vector(v0, v1);
+	int p1 = 2 * dv.x - dv.y;
+	int p2 = 2 * dv.z - dv.y;
+
+	while (v0.y != v1.y) {
+		v0.y += sv.y;
+		if (p1 >= 0) {
+			v0.x += sv.x;
+			p1 -= 2 * dv.y;
+		}
+		if (p2 >= 0) {
+			v0.z += sv.z;
+			p2 -= 2 * dv.y;
+		}
+		p1 += 2 * dv.x;
+		p2 += 2 * dv.z;
+		colorize_pixel(data, &v0, lerp_color(v0.color, v1.color, abs(v1.x - v0.x) + abs(v1.y - v0.y) + abs(v1.z - v0.z), dv.x + dv.y + dv.z));
+	}
+}
+
+static void	bresenham_z(t_data *data, t_int_vertex v0, t_int_vertex v1, t_int_vertex dv)
+{
+	const t_int_vertex	sv = get_shift_vector(v0, v1);
+	int p1 = 2 * dv.y - dv.z;
+	int p2 = 2 * dv.x - dv.z;
+
+	while (v0.z != v1.z)
+	{
+		v0.z += sv.z;
+		if (p1 >= 0) {
+			v0.y += sv.y;
+			p1 -= 2 * dv.z;
+		}
+		if (p2 >= 0) {
+			v0.x += sv.x;
+			p2 -= 2 * dv.z;
+		}
+		p1 += 2 * dv.y;
+		p2 += 2 * dv.x;
+		colorize_pixel(data, &v0, lerp_color(v0.color, v1.color, abs(v1.x - v0.x) + abs(v1.y - v0.y) + abs(v1.z - v0.z), dv.x + dv.y + dv.z));
+	}
+}
+
+void	bresenham(t_data *data, t_int_vertex v0, t_int_vertex v1)
+{
+	const t_int_vertex	dv = {
+		abs(v1.x - v0.x),
+		abs(v1.y - v0.y),
+		abs(v1.z - v0.z),
+		0};
+
+	colorize_pixel(data, &v0, v0.color);
+	if (dv.x >= dv.y && dv.x >= dv.z)
+		bresenham_x(data, v0, v1, dv);
+	else if (dv.y >= dv.x && dv.y >= dv.z)
+		bresenham_y(data, v0, v1, dv);
+	else
+		bresenham_z(data, v0, v1, dv);
 }
 
 void	put_lines(t_data *data, t_vertex **copy)
