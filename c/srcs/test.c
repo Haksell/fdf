3ea0883f3491086e5fc6ce1331caf2c8e97358d9
Haksell <axel.brisse@gmail.com>
2025-03-30 16:46:54 +0200

    .

--- c/srcs/test.c
+++ c/srcs/test.c
@@ -8,15 +8,13 @@
 #define VALID_DIRECTORY "../maps/valid/"
 #define INVALID_DIRECTORY "../maps/invalid/"
 
-void display_title(char* s) { ft_printf(BOLDBLUE "--- %s ---\n" RESET, s); }
-
-void ft_assert(char* test_name, bool result, bool expected) {
+static void ft_assert(char* test_name, bool result, bool expected) {
     char* color = result == expected ? GREEN : RED;
     char* verdict = result == expected ? "OK" : "KO";
     ft_printf("%s%s: %s\n" RESET, color, test_name, verdict);
 }
 
-t_list* ls(char* path) {
+static t_list* ls(char* path) {
     t_list* lst = NULL;
     DIR* dp = opendir(path);
     struct dirent* ep = readdir(dp);
@@ -29,8 +27,8 @@ t_list* ls(char* path) {
     return (lst);
 }
 
-void test(char* title, char* directory, bool expected) {
-    display_title(title);
+static void test(char* title, char* directory, bool expected) {
+    ft_printf(BOLDBLUE "--- %s ---\n" RESET, title);
     t_list* filenames = ls(directory);
     if (!expected) ft_lstadd_front(&filenames, ft_lstnew("../maps/invalid/notfound.fdf"));
     while (filenames != NULL) {
